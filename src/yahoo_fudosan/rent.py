81017c8e2d3fc5067f44a8d1df719addcbc8a608
Gary Sentosa <gary.sentosa@gmail.com>
2020-07-06 10:34:38 +0900

    Extract other rental cost

--- src/yahoo_fudosan/rent.py
+++ src/yahoo_fudosan/rent.py
@@ -5,7 +5,7 @@ def _ignore_exceptions(function):
     def wrapper(*args, **kwargs):
         try:
             return function(*args, **kwargs)
-        except (AttributeError, TypeError):
+        except (AttributeError, IndexError, TypeError):
             return ''
 
     return wrapper
@@ -13,11 +13,17 @@ def _ignore_exceptions(function):
 
 class RentListing(PropertyListing):
     def extract_rent_data(self):
-        return {
-            'cost': {
-                'rent_price': self._extract_rent_price(),
-            },
+        rent_data = {}
+
+        rent_data['cost'] = {
+            'rent_price': self._extract_rent_price(),
+            'monthly_fee': self._extract_other_rental_cost(0, 0),
+            'bond_deposit': self._extract_other_rental_cost(1, 0),
+            'key_money': self._extract_other_rental_cost(1, 1),
+            'security_deposit': self._extract_other_rental_cost(1, 2),
         }
+
+        return rent_data
 
     @_ignore_exceptions
     def _extract_rent_price(self):
@@ -25,5 +31,15 @@ class RentListing(PropertyListing):
             self._soup
             .find('dl', class_='priceData')
             .find('dd', class_='price')
+            .get_text(strip=True)
+        )
+
+    @_ignore_exceptions
+    def _extract_other_rental_cost(self, dl_tag_index, span_tag_index):
+        return (
+            self._soup
+            .find_all('dl', class_='priceData')[dl_tag_index]
+            .find('dd', class_='sub')
+            .find_all('span')[span_tag_index]
             .get_text(strip=True)
         )
--- tests/test_rent_listing.py
+++ tests/test_rent_listing.py
@@ -5,3 +5,7 @@ def test_extract_rent_data(rent_listing):
     rent_data = rent_listing.extract_rent_data()
 
     check.equal(rent_data['cost']['rent_price'], '18.5万円')
+    check.equal(rent_data['cost']['monthly_fee'], '11,000円')
+    check.equal(rent_data['cost']['bond_deposit'], '18.5万円')
+    check.equal(rent_data['cost']['key_money'], '18.5万円')
+    check.equal(rent_data['cost']['security_deposit'], 'なし')
