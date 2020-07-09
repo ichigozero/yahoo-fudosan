ac9b8e13143c845873bbfafd3f1f95369c9ab232
Gary Sentosa <gary.sentosa@gmail.com>
2020-07-09 10:44:03 +0900

    Extract house location

--- src/yahoo_fudosan/house.py
+++ src/yahoo_fudosan/house.py
@@ -17,6 +17,7 @@ class HouseListing(PropertyListing):
     def extract_house_data(self):
         return {
             'house_price': self._extract_house_price(),
+            'house_location': self._extract_house_location(),
         }
 
     @_ignore_exceptions
@@ -26,5 +27,25 @@ class HouseListing(PropertyListing):
             .find('th', string=re.compile('価格'))
             .find_next_sibling('td')
             .find('span')
+            .get_text(strip=True)
+        )
+
+    def _extract_house_location(self):
+        table_data = self._extract_table_data('所在地')
+        return table_data[:table_data.find('\n')]
+
+    @_ignore_exceptions
+    def _extract_table_data(self, row_header_title):
+        found_tag = self._soup.find('th', string=re.compile(row_header_title))
+        if found_tag is None:
+            found_tag = (
+                self._soup
+                .find('span', string=re.compile(row_header_title))
+                .parent
+            )
+
+        return (
+            found_tag
+            .find_next_sibling('td')
             .get_text(strip=True)
         )
--- tests/test_house_listing.py
+++ tests/test_house_listing.py
@@ -5,9 +5,11 @@ def test_extract_house_data(house_listing_ag):
     house_data = house_listing_ag.extract_house_data()
 
     check.equal(house_data['house_price'], '2,580万円')
+    check.equal(house_data['house_location'], '千葉県船橋市緑台2丁目')
 
 
 def test_extract_alternative_house_data(house_listing_corp):
     house_data = house_listing_corp.extract_house_data()
 
     check.equal(house_data['house_price'], '3,880万円')
+    check.equal(house_data['house_location'], '千葉県船橋市東船橋3丁目')
