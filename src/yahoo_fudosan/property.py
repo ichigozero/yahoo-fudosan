a1e05f23e548c4e541efdc7c245572694d721b90
Gary Sentosa <gary.sentosa@gmail.com>
2020-09-16 12:33:23 +0900

    Output timed rotating log

--- src/yahoo_fudosan/property.py
+++ src/yahoo_fudosan/property.py
@@ -1,8 +1,12 @@
+import logging
+import logging.config
 import requests
 import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+
+logger = logging.getLogger(__name__)
 
 
 class PropertySearch:
@@ -41,14 +45,26 @@ class PropertyListing:
         try:
             content = requests.get(url).content
             self._soup = BeautifulSoup(content, 'html.parser')
+            logger.info('Fetched %s', url)
         except requests.exceptions.RequestException:
             if retry_count < max_retry:
+                if retry_count == 0:
+                    logger.warning('Attempting to refetch %s', url)
+
+                logger.warning(
+                    'Refetch attempt %d of %d',
+                    retry_count + 1,
+                    max_retry
+                )
                 time.sleep(retry_delay)
                 self.get_soup(url, retry_count + 1, max_retry, retry_delay)
             else:
                 # Prevent scraping same page
                 # if subsequent URL requests fail
                 self._soup = None
+
+                logger.error('Unable to fetch the page')
+                logger.error('The maximum retry count has been exceeded')
 
         self._requested_url = url
 
